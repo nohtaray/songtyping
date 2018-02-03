c82a90d1165055b932c32370d1e5dd758e65c8b5
Yada <ydt.hran2@gmail.com>
2018-02-04 01:04:56 +0900

    タイピングできるようにする

--- src/index.js
+++ src/index.js
@@ -6,28 +6,89 @@ import './index.css';
 import {KeysBox, LyricBox} from './components.jsx';
 
 const lyricPages = [
-  [
-    {kanji: '私の恋を', hiragana: 'わたしのこいを'},
-    {kanji: '悲劇のジュリエットにしないで', hiragana: 'ひげきのじゅりえっとにしないで'},
-    {kanji: 'ここから連れ出して…', hiragana: 'ここからつれだして・・・'},
-    {kanji: 'そんな気分よ', hiragana: 'そんなきぶんよ'},
-  ],
+  {
+    lyrics: [
+      {kanji: '私の恋を', hiragana: 'わたしのこいを'},
+      {kanji: '悲劇のジュリエットにしないで', hiragana: 'ひげきのじゅりえっとにしないで'},
+      {kanji: 'ここから連れ出して…', hiragana: 'ここからつれだして・・・'},
+      {kanji: 'そんな気分よ', hiragana: 'そんなきぶんよ'},
+    ],
+  },
 ];
-const lyricPageCharPoses = [7, 6, 0, 0];
-const keys = 'higekinozyuriettonisinaide'.toUpperCase();
-const keysCharPos = 12;
+const tsuikyo = new window.Tsuikyo({flex: 'flex', prevent: true, im: 'roma'});
+tsuikyo.listen();
 
 class Game extends React.Component {
+  static propTypes = {
+    lyricPages: PropTypes.arrayOf(
+        PropTypes.shape({
+          lyrics: PropTypes.arrayOf(PropTypes.object).isRequired,
+        }),
+    ).isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lyricPageCharPoses: Array(4).fill(0),
+      pageCount: 0,
+      rowCount: 0,
+      keysPos: 0,
+      keys: '',
+    };
+
+    // マウントされるまでちょっと待つ。
+    // TODO: ボタンかなにかをトリガにする
+    setTimeout(() => {
+      this.startRowTypingIfNeeded();
+    }, 100);
+  }
+
+  startRowTypingIfNeeded() {
+    const lyricRow = this.props.lyricPages[this.state.pageCount].lyrics[this.state.rowCount];
+    if (lyricRow == null) return;
+
+    const tw = tsuikyo.make(lyricRow.hiragana);
+    this.updatePos(tw);
+    tw.listen(e => this.handleType(e, tw));
+  };
+
+  handleType(e, tw) {
+    this.updatePos(tw);
+
+    if (e.finish) {
+      tw.sleep();
+      this.setState({
+        rowCount: this.state.rowCount + 1,
+        keys: '',
+        keysPos: 0,
+      });
+      this.startRowTypingIfNeeded();
+    }
+  }
+
+  updatePos(tw) {
+    const lyricPageCharPoses = this.state.lyricPageCharPoses.slice();
+    lyricPageCharPoses[this.state.rowCount] = tw.pos();
+
+    this.setState({
+      lyricPageCharPoses,
+      keysPos: tw.kpos(),
+      keys: tw.kstr(),
+    });
+  }
+
   render() {
     return (
-        <div>
+        <div className="game">
           <LyricBox
-              lyrics={lyricPages[0]}
-              charPoses={lyricPageCharPoses}
+              lyrics={this.props.lyricPages[this.state.pageCount].lyrics}
+              charPoses={this.state.lyricPageCharPoses}
           />
           <KeysBox
-              keys={keys}
-              charPos={keysCharPos}
+              keys={this.state.keys}
+              charPos={this.state.keysPos}
           />
         </div>
     );
@@ -38,7 +99,7 @@ class Game extends React.Component {
 
 ReactDOM.render(
     (
-        <Game />
+        <Game lyricPages={lyricPages} />
     ),
     document.getElementById('root'),
 );
