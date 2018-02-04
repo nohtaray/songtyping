4811bd460fb33daf32092df90e296d7b84a582ad
Yada <ydt.hran2@gmail.com>
2018-02-04 17:37:38 +0900

    LyricLoader クラスを切り出す

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import 'normalize.css';
 import './index.css';
 import {Game} from './game.jsx';
+import {LyricLoader} from './LyricLoader.jsx';
 
 const audioSrc = '/audio/romeo_and_cinderella.mp3';
 const lyricSrc = '/lyric/romeo_and_cinderella.xml';
@@ -36,47 +37,6 @@ class AudioPlayer extends React.Component {
   }
 }
 
-class LyricLoader extends React.Component {
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    onLoad: PropTypes.func.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-
-    fetch(this.props.src).then(res => res.text()).then(xml => {
-      const lyric = this.parseXml(xml);
-      this.props.onLoad(lyric);
-    });
-  }
-
-  parseXml(xml) {
-    const dom = (new DOMParser()).parseFromString(xml, 'text/xml');
-    const pages = dom.querySelectorAll('data');
-
-    return [].map.call(pages, (page) => {
-          window.page = page;
-          const time = page.querySelector('time').textContent * 10;
-          const kanjiStr = page.querySelector('lyric').textContent;
-          const hiraganaStr = page.querySelector('lyric_type').textContent;
-
-          const kanjis = kanjiStr !== '' ? kanjiStr.split('*', 4) : [];
-          const hiraganas = hiraganaStr !== '' ? hiraganaStr.split('*', 4) : [];
-          return {
-            time: time,
-            lyrics: hiraganas.map((h, i) => {
-              return {hiragana: h, kanji: kanjis[i]};
-            }),
-          };
-        },
-    );
-  }
-
-  render() {
-    return '';
-  }
-}
 
 // TODO: このクラス Game の中に入れる
 class LyricViewer extends React.Component {
