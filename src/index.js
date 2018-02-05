a9c6ad8c127c6bca097f4beef5e44d6eb470f82f
Yada <ydt.hran2@gmail.com>
2018-02-06 00:38:15 +0900

    空の reducer と root component を追加

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import {Game} from './game.jsx';
+import App from './components/App';
+import {createStore} from 'redux';
+import reducer from './reducer';
+import {Provider} from 'react-redux';
 
 const audioSrc = '/audio/romeo_and_cinderella.mp3';
 const lyricSrc = '/lyric/romeo_and_cinderella.xml';
 
 // ========================================
 
+let store = createStore(reducer);
+
 ReactDOM.render(
     (
         <div>
+          <Provider store={store}>
+            <App />
+          </Provider>
+          {/* あとで消す */}
           <Game lyricSrc={lyricSrc} audioSrc={audioSrc} />
         </div>
     ),
