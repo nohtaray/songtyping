08bce0d93d8be57c6553597f22b099aff196f2d2
Yada <ydt.hran2@gmail.com>
2018-02-09 00:44:31 +0900

    不要になったファイルを削除

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import {Game} from './game.jsx';
 import App from './components/App';
 import reducer from './reducer';
 import {Provider} from 'react-redux';
@@ -13,9 +12,6 @@ import logger from 'redux-logger';
 import mySaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-
-const audioSrc = '/audio/romeo_and_cinderella.mp3';
-const lyricSrc = '/lyric/romeo_and_cinderella.xml';
 
 // ========================================
 
@@ -32,8 +28,6 @@ ReactDOM.render(
           <Provider store={store}>
             <App />
           </Provider>
-          {/* あとで消す */}
-          {/*<Game lyricSrc={lyricSrc} audioSrc={audioSrc} />*/}
         </div>
     ),
     document.getElementById('root'),
